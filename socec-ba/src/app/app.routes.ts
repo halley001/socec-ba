17d518516b8d67e3770c73bf40fe921dc37d422d
halley001 <halleymail100@gmail.com>
2023-12-06 12:30:22 +0100

    site structure/layout/draft

--- socec-ba/src/app/app.routes.ts
+++ socec-ba/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { BlogComponent } from './components/blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component:HomeComponent},
+    {path:'contact', component:ContactComponent},
+    {path:'services', component:ServicesComponent},
+    {path:'testimonials', component:TestimonialsComponent},
+    {path:'about', component:AboutComponent},
+    {path:'team', component:TeamComponent},
+    {path:'blog', component:BlogComponent} ,
+    {path:'', component:PagenotfoundComponent},
+    {path: '**', redirectTo: 'home', pathMatch:'full'},
+];
